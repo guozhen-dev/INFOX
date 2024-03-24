bba804416838960dcbd8d971548dce2273953c65
Guozhen Ding <mail@guozhen.dev>
2024-03-24 17:32:09 -0400

    Someone push -f. :((((( SO BADDDD.

--- chrome/fork-insight/build.sh
+++ chrome/fork-insight/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install
-NODE_OPTIONS=--openssl-legacy-provider npm run build
+CI=false NODE_OPTIONS=--openssl-legacy-provider npm run build
 rm -rf ../build
 cp -r build ../chrome-extension
